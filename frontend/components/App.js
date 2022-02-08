f013a27c09983f642aba25ab91b45561ebaccb9c
Schuyler <skym1n0r91@gmail.com>
2022-02-08 15:28:11 -0600

    cleaning up

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,20 +1,72 @@
 import React from "react";
+import TodoList from "./TodoList";
+import Form from "./Form";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: "Organize Garage",
+          id: 1528817077286,
+          completed: false,
+        },
+        {
+          name: "Bake Cookies",
+          id: 1528817084358,
+          completed: false,
+        },
+      ],
+    };
+  }
+
+  handleAdd = (name) => {
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false,
+    };
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter((todo) => {
+        return todo.completed === false;
+      }),
+    });
+  };
+
+  handleToggle = (clickedId) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === clickedId) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        } else {
+          return todo;
+        }
+      }),
+    });
+  };
+
   render() {
+    const { todos } = this.state;
+
     return (
       <div>
         <h2>Todo</h2>
-        <ul>
-          <li>Walk the dog</li>
-          <li>Take out the trash</li>
-          <li>Workout</li>
-        </ul>
-        <form>
-          <input />
-          <button>Add</button>
-        </form>
-        <button>Clear</button>
+        <TodoList handleToggle={this.handleToggle} todos={todos} />
+        <Form handleAdd={this.handleAdd} />
+        <button onClick={this.handleClear}>Clear</button>
       </div>
     );
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.handleAdd(this.state.input);
+  };
+  handleChange = (event) => {
+    this.setState({ ...this.state, input: event.target.value });
+  };
   render() {
     return (
-      <div>
-        Form
-      </div>
-    )
+      <form>
+        <input
+          onChange={this.handleChange}
+          type="text"
+          placeholder="type text"
+        />
+        <button onClick={this.handleSubmit}>Add</button>
+      </form>
+    );
   }
 }
