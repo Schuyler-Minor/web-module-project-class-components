36e729363a53423134e9394cf283752d951d8ba2
Schuyler <skym1n0r91@gmail.com>
2022-02-07 22:33:44 -0600

    form

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h2>Todo</h2>
+        <ul>
+          <li>Walk the dog</li>
+          <li>Take out the trash</li>
+          <li>Workout</li>
+        </ul>
+        <form>
+          <input />
+          <button>Add</button>
+        </form>
+        <button>Clear</button>
       </div>
-    )
+    );
   }
 }
